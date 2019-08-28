fd6621235869fd4b8cba49163d6b4fb4aa14879d
RadenMahar <panji@alterra.id>
2019-08-28 22:08:01 +0700

    cobaaaa

---
+++ build/precache-manifest.0fea122cc1787b17118c9d9e2ddea431.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c66a84a5f1ee8c197984f6d9e37c489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eab83f25691eb6ff88bd",
+    "url": "/static/css/main.5efec137.chunk.css"
+  },
+  {
+    "revision": "bb34a41760f081997fa4",
+    "url": "/static/js/2.b8c4032a.chunk.js"
+  },
+  {
+    "revision": "eab83f25691eb6ff88bd",
+    "url": "/static/js/main.b9b0bc31.chunk.js"
+  },
+  {
+    "revision": "d525babdca1a92eb0bcd",
+    "url": "/static/js/runtime~main.c76de6cf.js"
+  },
+  {
+    "revision": "d44236620e188ff0f4029e99d8e81afc",
+    "url": "/static/media/barang.d4423662.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d831b4fefb27e676757ff062866830a3",
+    "url": "/static/media/tesla.d831b4fe.jpg"
+  }
+]);
